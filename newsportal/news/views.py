fdd97de84a5aeb2cd15ca71a130cd114e9257686
Serg9t <ssokurr@mail.ru>
2023-06-07 20:47:11 +0300

    added new file

---
+++ newsportal/news/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
